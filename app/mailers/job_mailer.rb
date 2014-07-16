de4a861dc828340dacf721a83ba04e804abeb1fa
jsleetw <ail529@gmail.com>
2014-07-16 14:43:34 +0800

    wording fix

--- app/mailers/job_mailer.rb
+++ app/mailers/job_mailer.rb
@@ -1,9 +1,9 @@
 class JobMailer < ActionMailer::Base
 
-  default from: "\"66Kjobs\" #{Setting.email_sender}"
+  default from: "\"百萬案件發包網\" #{Setting.email_sender}"
 
   def deliver_email_for_edit(job)
     @job = job
-    mail(:to => job.email, :subject => "[66Kjobs] 感謝您在本站刊登工作！")
+    mail(:to => job.email, :subject => "[百萬案件發包網] 感謝您在本站刊登案件！")
   end
 end
